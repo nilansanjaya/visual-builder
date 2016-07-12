8c2575a8ca9043724bca296b02fa5dffbf901be4
Nilan Sanjaya <nilansanjaya@gmail.com>
2016-07-12 12:45:29 +0530

    Editable snippets

--- visual-builder.js
+++ visual-builder.js
@@ -2,10 +2,23 @@ var app_container = "#app_container";
 var app_width = '800px';
 var app_height = '1000px';
 
+var snippet_list = [
+    {
+        "name": "Header",
+        "id": "header"
+    },
+    {
+        "name": "Paragraph",
+        "id": "paragraph"
+    }
+];
+
 $(document).ready(function(){
     initApp();
-    $( "#app_container" ).sortable();
     initSnippets();
+
+    $( "#app_container" ).sortable({cancel: ':input,button,[contenteditable]'});
+
 });
 
 function initApp(){
@@ -13,6 +26,10 @@ function initApp(){
     $(app_container).css({
         width: app_width,
         minHeight: app_height,
+    });
+
+    $.each( snippet_list, function( key, value ) {
+        $("#snippet_list").append("<option value='"+value.id+"'>"+value.name+"</option>")
     });
 
 }
@@ -27,8 +44,6 @@ function initSnippets(){
 
 }
 
-
-
 function loadSnippet(snippetName,element){
 
     $.get( "snippets/"+snippetName+".txt", function( data ) {
@@ -36,5 +51,17 @@ function loadSnippet(snippetName,element){
         $(element).html( data );
 
     });
+
+}
+
+function addSnippet(){
+
+    var snippet_id =  $("#snippet_list").val();
+
+    var el = $('<div class="object"></div>');
+
+    $(app_container).append(el).attr("snippet-type", snippet_id);
+
+    loadSnippet(snippet_id, el);
 
 }
